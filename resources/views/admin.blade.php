72e7ded900aa89439f0c128497e3a1df9e7ad29a
Nick de Kruijk <git@nickdekruijk.nl>
2024-04-22 22:36:24 +0200

    Update admin view

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -19,6 +19,14 @@
             border: 1px solid #bbb;
             padding: 5px 10px;
         }
+
+        TD.right {
+            text-align: right;
+        }
+
+        TD.center {
+            text-align: center;
+        }
     </style>
     <section class="fullpage">
         <div class="visitors">
@@ -29,7 +37,7 @@
                 @foreach (Visitors::yearlyVisitors() as $year)
                     <tr>
                         <td>{{ $year->year }}</td>
-                        <td>{{ $year->visitors }}</td>
+                        <td class="right">{{ $year->visitors }}</td>
                     </tr>
                 @endforeach
             </table>
@@ -40,7 +48,7 @@
                 @foreach (Visitors::monthlyVisitors() as $month)
                     <tr>
                         <td>{{ ucfirst($month->created_at->isoFormat('MMMM YYYY')) }}</td>
-                        <td>{{ $month->visitors }}</td>
+                        <td class="right">{{ $month->visitors }}</td>
                     </tr>
                 @endforeach
             </table>
@@ -51,25 +59,31 @@
                 @foreach (Visitors::dailyVisitors() as $month)
                     <tr>
                         <td>{{ ucfirst($month->created_at->isoFormat('dd D MMMM YYYY')) }}</td>
-                        <td>{{ $month->visitors }}</td>
+                        <td class="right">{{ $month->visitors }}</td>
                     </tr>
                 @endforeach
             </table>
             <table>
                 <tr>
-                    <th>Laatste 10</th>
+                    <th>Afgelopen uur</th>
                     <th>IP adres</th>
                     <th>Hardware</th>
                     <th>OS</th>
                     <th>Browser</th>
+                    <th>Method</th>
+                    <th>Screen</th>
+                    <th>Viewport</th>
                 </tr>
-                @foreach (Visitors::latestVisitors(10) as $visitor)
+                @foreach (Visitors::latestVisitors(now()->subMinutes(60)) as $visitor)
                     <tr>
                         <td>{{ $visitor->created_at->isoFormat('dd D MMM YYYY HH:mm') }}</td>
-                        <td>{{ trim($visitor->ip, '.0') }}.?</td>
+                        <td class="right">{{ trim($visitor->ip, '.0') }}.?</td>
                         <td>{{ $visitor->hardware }}</td>
                         <td>{{ $visitor->platform }}</td>
                         <td>{{ $visitor->browser }}</td>
+                        <td class="center">{{ $visitor->javascript ? 'Js' : '' }} {{ $visitor->pixel ? 'Px' : '' }}</td>
+                        <td class="center">{{ $visitor->screen_width }} x {{ $visitor->screen_height }}{{ $visitor->screen_pixel_ratio ? ' @' . $visitor->screen_pixel_ratio . 'x' : '' }}</td>
+                        <td class="center">{{ $visitor->viewport_width }} x {{ $visitor->viewport_height }}</td>
                     </tr>
                 @endforeach
             </table>
